c620f9844bff0f8637498945e27a43c5e0ffc5fc
James Rakich <james@fullandbydesign.com.au>
2018-06-11 15:28:14 +1000

    Stub out potential dashboard API.

--- usage/anathema.js
+++ usage/anathema.js
@@ -4,8 +4,6 @@ require('./styles.js')
 require('./dependencies.js')
 
 anathema.dashboard("default", function (dashboard) {
-  dashboard.addWatch('styles')
-  console.log("running dashboard")
 })
 
 module.exports = anathema
